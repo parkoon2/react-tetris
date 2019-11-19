b4a842629b92950a2d87d03241233d1a629643b8
parkoon <par_koon@naver.com>
2019-11-19 10:48:19 +0900

    Updates

---
+++ precache-manifest.de04a388ef8b1901cb6f790a381b613b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8d8528635b3b8147088fef9e22f107",
+    "url": "/react-tetris/index.html"
+  },
+  {
+    "revision": "101ff1b778eeb03d98a8",
+    "url": "/react-tetris/static/css/main.13370a51.chunk.css"
+  },
+  {
+    "revision": "984e70a001b6d7d6e603",
+    "url": "/react-tetris/static/js/2.0c2de8bb.chunk.js"
+  },
+  {
+    "revision": "101ff1b778eeb03d98a8",
+    "url": "/react-tetris/static/js/main.6abf8ad7.chunk.js"
+  },
+  {
+    "revision": "029efe1d95d0c6250d6f",
+    "url": "/react-tetris/static/js/runtime-main.b4b75ff2.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/react-tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/react-tetris/static/media/bg.2810fe6b.png"
+  }
+]);
