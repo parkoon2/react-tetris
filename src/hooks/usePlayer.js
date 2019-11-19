98b4ed493cc3ec5090cf2db6e4e986956c3bd7f2
parkoon <par_koon@naver.com>
2019-11-19 10:54:09 +0900

    updated: remove first block before start

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,12 +1,12 @@
 import { useState, useCallback } from "react";
 
-import { randomTetromino } from "../tetrominos";
+import { randomTetromino, TETROMINOS } from "../tetrominos";
 import { STAGE_WIDTH, checkCollision } from "../gameHelpers";
 
 export const usePlayer = () => {
   const [player, setPlayer] = useState({
     pos: { x: 0, y: 0 },
-    tetromino: randomTetromino().shape,
+    tetromino: TETROMINOS[0].shape,
     collided: false
   });
 
